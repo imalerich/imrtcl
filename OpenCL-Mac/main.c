9e61be32cc8ef45e94603ff3c48b89f3eb71d3f6
Ian Malerich <imm@mac-air-08.local>
2016-01-16 20:53:15 -0600

    Initial Commit

---
+++ OpenCL-Mac/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OpenCL-Mac
+//
+//  Created by Ian Malerich on 1/16/16.
+//  Copyright © 2016 Ian Malerich. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
